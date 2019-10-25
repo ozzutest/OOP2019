05a8ac806e0bb275883f8aa05a70f09690b5239d
ozzutest <47740803+ozzutest@users.noreply.github.com>
2019-10-25 14:32:09 +0200

    Add files via upload

---
+++ Wejsciowka02/Wejsciowka02/Miasto/Miasto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wejsciowka02.Miasto
+{
+    class Miasto
+    {
+        public int liczbaLudnosci;
+        public string nazwaMiasta;
+
+        public Miasto(int liczbaLudnosci, string nazwaMiasta)
+        {
+            this.liczbaLudnosci = liczbaLudnosci;
+            this.nazwaMiasta = nazwaMiasta;
+        }
+        public string PodajCeneZaMetr()
+        {
+            if (liczbaLudnosci == 1000)
+                return "Cena za metr^2 mieszknia: 5000zl";
+            else if (liczbaLudnosci == 2000)
+                return "Cena za metr^2 mieszkania: 10000zl";
+
+            return "Wrong data!";
+        }
+    }
+}
---
+++ Wejsciowka02/Wejsciowka02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Wejsciowka02.Miasto
+{
+    namespace Wejsciowka02
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Miasto obiekt = new Miasto(1000, "Olsztyn");
+                
+                Console.WriteLine($"{obiekt.nazwaMiasta}");
+                Console.WriteLine(obiekt.PodajCeneZaMetr());
+            }
+        }
+    }
+}
+
+
