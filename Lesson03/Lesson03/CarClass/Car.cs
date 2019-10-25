9fa351e347b30078124f95417f7dd0dc796ce9c8
ozzutest <47740803+ozzutest@users.noreply.github.com>
2019-10-25 16:03:25 +0200

    Add files via upload

---
+++ Lesson03/Lesson03/CarClass/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson03.CarClass
+{
+    class Car
+    {
+        static int iloscKol = 4;
+        private double pojemnoscSilnika;
+        private string marka;
+
+        public double PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { value = pojemnoscSilnika; }
+        }
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+
+        public string Marka
+        {
+            get { return marka; }
+            set { value = marka; }
+        }
+
+        public static int IloscKol
+        {
+            get {return iloscKol; }
+            set { value = iloscKol; }
+        }
+
+        public Car CreateCar()
+        { 
+            return new Car(this.pojemnoscSilnika, this.marka);
+        }
+    }
+}
---
+++ Lesson03/Lesson03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Lesson03.CarClass;
+
+namespace Lesson03
+{
+     class Program
+     {
+         static void Main(string[] args)
+         {
+            Car car = new Car(4000, "Lexus");
+            Car car4 = new Car(5000, "Lamborghini");
+            //2.nie mozna
+            Console.WriteLine($"{car.PojemnoscSilnika}, {car.Marka}");
+            Car car2 = car.CreateCar();
+            Console.WriteLine($"{car2.PojemnoscSilnika}, {car2.Marka}");
+            Car car3 = car4.CreateCar();
+            Console.WriteLine($"{car4.PojemnoscSilnika}, {car4.Marka}");
+
+            Console.WriteLine(Car.IloscKol);
+
+            
+         }
+     }
+}
+
+
